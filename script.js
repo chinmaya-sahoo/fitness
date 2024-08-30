408537c00fcd22366ab131cc9badeb045b403d87
chinmaya-sahoo <148933538+chinmaya-sahoo@users.noreply.github.com>
2024-08-30 15:34:15 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+
+// JavaScript for toggling sidebar
+const hamburger = document.querySelector('.hamburger');
+const sidebar = document.querySelector('.sidebar');
+
+hamburger.addEventListener('click', () => {
+    sidebar.classList.toggle('active');
+});
